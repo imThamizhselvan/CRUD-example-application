798665a85f8961dcd38d6d929c38fa69a6005433
thamizhselvan <thamizhtechie@gmail.com>
2019-03-28 16:32:25 +0530

    Updates

---
+++ precache-manifest.ffe59ae9422865d90bf598289db6fa57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5935f61674f8726a6b73",
+    "url": "/CRUD-example-application/static/js/runtime~main.a1670ec6.js"
+  },
+  {
+    "revision": "2bdda18b13589f9bd439",
+    "url": "/CRUD-example-application/static/js/main.7a36a42a.chunk.js"
+  },
+  {
+    "revision": "5c2b56edf5a732925038",
+    "url": "/CRUD-example-application/static/js/2.aa7616a2.chunk.js"
+  },
+  {
+    "revision": "2bdda18b13589f9bd439",
+    "url": "/CRUD-example-application/static/css/main.e0808af9.chunk.css"
+  },
+  {
+    "revision": "13c7097c0583e8bd7cfe6fe86555408a",
+    "url": "/CRUD-example-application/index.html"
+  }
+];
